c02cbc7712e14bf2347ece3761efdb8a2431328a
Juan Bedoya <jbedoyac10@gmail.com>
2024-10-11 10:12:39 -0600

    add ssh connection

--- main.py
+++ main.py
@@ -5,93 +5,167 @@ from pymongo import MongoClient
 import os
 import httpx
 import asyncio
+import paramiko
+import base64
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 client = MongoClient(MONGO_URI)
-db = client[os.getenv("MONGO_DB_NAME", "logDB")]
-collection = db[os.getenv("MONGO_COLLECTION_NAME", "requestsLogs")]
+db = client[os.getenv("MONGO_DB_NAME")]
+collection = db[os.getenv("MONGO_COLLECTION_NAME")]
 
-REST_SERVER_URL = os.getenv("REST_SERVER_URL")
+REST_SERVER_BASE_URL = os.getenv("REST_SERVER_BASE_URL")
 AUTHORIZATION_HEADER = os.getenv("AUTHORIZATION_HEADER")
 CONTENT_TYPE = os.getenv("CONTENT_TYPE")
 
+SSH_HOST = os.getenv("SSH_HOST")
+SSH_PORT = int(os.getenv("SSH_PORT", 22))
+SSH_USERNAME = os.getenv("SSH_USERNAME")
+SSH_PASSWORD = os.getenv("SSH_PASSWORD")
+
 app = FastAPI()
 
-async def send_message_async(client, xml_body):
-    response = await client.post(
-        REST_SERVER_URL,
-        headers={"Authorization": AUTHORIZATION_HEADER, "Content-Type": CONTENT_TYPE},
-        content=xml_body
-    )
+logging.basicConfig(level=logging.INFO)
+
+def send_via_ssh(xml_body: str, endpoint: str):
+    try:
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        logging.info(f"Connecting to SSH host {SSH_HOST}:{SSH_PORT} with username {SSH_USERNAME}")
+        ssh_client.connect(SSH_HOST, SSH_PORT, SSH_USERNAME, SSH_PASSWORD)
+        logging.info("SSH connection established successfully")
+
+        url = f"{REST_SERVER_BASE_URL}/{endpoint}"
+
+        command = f"""curl --location '{url}' \
+        --header 'Authorization: {AUTHORIZATION_HEADER}' \
+        --header 'Content-Type: {CONTENT_TYPE}' \
+        --data '{xml_body}'"""
+
+        logging.info(f"Executing command via SSH: {command}")
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+
+        response = stdout.read().decode()
+        ssh_client.close()
+
+        logging.info("SSH command executed successfully, response received.")
+        return response
+
+    except Exception as e:
+        logging.error(f"SSH connection or command failed: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"SSH connection or command failed: {str(e)}")
+
+def encode_message_in_base64(xml_body: str) -> str:
+    xml_dict = xmltodict.parse(xml_body)
     
-    collection.insert_one({
-        "xml_body": xml_body.decode('utf-8'),
-        "response_status_code": response.status_code,
-        "response_text": response.text
-    })
-    
-    return response.text
+    message = xml_dict['AoSmsRequest']['AoSms'].get('Message')
+    if message:
+        encoded_message = base64.b64encode(message.encode('utf-8')).decode('utf-8')
+        xml_dict['AoSmsRequest']['AoSms']['Message'] = encoded_message
+
+    return xmltodict.unparse(xml_dict, pretty=True)
 
 @app.post("/send_message")
 async def send_message(request: Request):
-    xml_body = await request.body()
+    try:
+        xml_body = await request.body()
+        
+        xml_body_encoded = encode_message_in_base64(xml_body.decode('utf-8'))
 
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            REST_SERVER_URL,
-            headers={"Authorization": AUTHORIZATION_HEADER, "Content-Type": CONTENT_TYPE},
-            content=xml_body
-        )
-    collection.insert_one({
-        "xml_body": xml_body.decode('utf-8'),
-        "response_status_code": response.status_code,
-        "response_text": response.text
-    })
-    return Response(content=response.content, media_type="application/xml")
+        response = send_via_ssh(xml_body.decode('utf-8'), "AoSms")
+        
+        log_entry = {
+            "xml_body": xml_body.decode('utf-8'),
+            "xml_body_encoded": xml_body_encoded,
+            "response": response
+        }
+        
+        print("Log Entry to be inserted:", log_entry)
+        
+        collection.insert_one(log_entry)
+        
+        return Response(content=response, media_type="application/xml")
+    except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/query_message")
 async def query_message(request: Request):
-    xml_body = await request.body()
-    response = requests.post(
-        REST_SERVER_URL,
-        headers={"Authorization": AUTHORIZATION_HEADER, "Content-Type": CONTENT_TYPE},
-        data=xml_body
-    )
-    return Response(content=response.content, media_type="application/xml")
+    try:
+        xml_body = await request.body()
+
+        response = send_via_ssh(xml_body.decode('utf-8'), "AoSmquery")
+
+        return Response(content=response, media_type="application/xml")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/cancel_message")
 async def cancel_message(request: Request):
-    xml_body = await request.body()
-    data = xmltodict.parse(xml_body)
-    message_id = data.get('AoSmcancelRequest', {}).get('AoSmcancel', {}).get('MessageId')
-    a_address = data.get('AoSmcancelRequest', {}).get('AoSmcancel', {}).get('aAddress')
+    try:
+        xml_body = await request.body()
+        data = xmltodict.parse(xml_body)
+        message_id = data.get('AoSmcancelRequest', {}).get('AoSmcancel', {}).get('MessageId')
+        a_address = data.get('AoSmcancelRequest', {}).get('AoSmcancel', {}).get('aAddress')
 
-    if not message_id or not a_address:
-        return Response(
-            content=xmltodict.unparse({
-                "AoSmcancelResponse": {
-                    "Code": "1",
-                    "Description": "Missing MessageId or aAddress",
-                    "ResultList": {
-                        "Result": {
-                            "Status": "1",
-                            "Description": "Invalid request, both MessageId and aAddress are required"
+        if not message_id or not a_address:
+            return Response(
+                content=xmltodict.unparse({
+                    "AoSmcancelResponse": {
+                        "Code": "1",
+                        "Description": "Missing MessageId or aAddress",
+                        "ResultList": {
+                            "Result": {
+                                "Status": "1",
+                                "Description": "Invalid request, both MessageId and aAddress are required"
+                            }
                         }
                     }
-                }
-            }, pretty=True), media_type="application/xml")
+                }, pretty=True), media_type="application/xml")
 
-    response = requests.post(
-        REST_SERVER_URL,
-        headers={"Authorization": AUTHORIZATION_HEADER, "Content-Type": CONTENT_TYPE},
-        data=xml_body
-    )
-    collection.update_one({"MessageId": int(message_id)}, {"$set": {"Status": "Cancelled", "Description": "Message cancelled"}})
-    return Response(content=response.content, media_type="application/xml")
+        response = send_via_ssh(xml_body.decode('utf-8'), "AoSmcancel")
 
+        collection.update_one(
+            { "response": { "$regex": message_id } },
+            { "$set": { "Description": "MESSAGE CANCELLED" } }
+        )
+        
+        return Response(content=response, media_type="application/xml")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+async def send_message_async(a_address: str, b_address: str, message: str, data_coding_scheme: int):
+    try:
+        xml_body = f"""
+            <AoSmsRequest>
+                <AoSms>
+                    <aAddress>{a_address}</aAddress>
+                    <bAddress>{b_address}</bAddress>
+                    <Message>{message}</Message>
+                    <DataCodingScheme>{data_coding_scheme}</DataCodingScheme>
+                </AoSms>
+            </AoSmsRequest>
+        """
+        xml_body_encoded = encode_message_in_base64(xml_body)
+        response = send_via_ssh(xml_body_encoded, "AoSms")
+
+        log_entry = {
+            "aAddress": a_address,
+            "bAddress": b_address,
+            "xml_body": xml_body,
+            "xml_body_encoded": xml_body_encoded,
+            "response": response
+        }
+
+        collection.insert_one(log_entry)
+
+        return {"bAddress": b_address, "response": response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 @app.post("/bulk_send")
 async def bulk_send(request: Request):
     try:
@@ -101,24 +175,13 @@ async def bulk_send(request: Request):
         message = body['Message']
         data_coding_scheme = body['DataCodingScheme']
 
-        async with httpx.AsyncClient() as client:
-            tasks = []
-            for b_address in b_addresses:
-                xml_body = f"""
-                    <AoSmsRequest>
-                        <AoSms>
-                            <aAddress>{a_address}</aAddress>
-                            <bAddress>{b_address}</bAddress>
-                            <Message>{message}</Message>
-                            <DataCodingScheme>{data_coding_scheme}</DataCodingScheme>
-                        </AoSms>
-                    </AoSmsRequest>
-                """
-                tasks.append(send_message_async(client, xml_body.encode('utf-8')))
+        tasks = []
 
-            results = await asyncio.gather(*tasks)
+        for b_address in b_addresses:
+            tasks.append(send_message_async(a_address, b_address, message, data_coding_scheme))
+
+        results = await asyncio.gather(*tasks)
 
         return {"results": results}
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
